8ad195810917c6b6cba6c399867de2adf1d941a6
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-14 16:15:04 -0500

    uncommented code

--- index.ts
+++ index.ts
@@ -14,31 +14,27 @@ clicks$.pipe(
   () => console.log('All done.')
 );
 
+let nums = [2, 4, 6, 9, 10];
 
+let numsObservable$ = from(nums);
 
+let evenNumbers$ = Observable.create(subscriber => {
 
+  for (let currentNum of nums) {
+    if (currentNum % 2 === 0) {
+      subscriber.next(currentNum);
+    }
+    else {
+      subscriber.error('Value is not even.');
+    }
+  }
 
-// let nums = [2, 4, 6, 9, 10];
-
-// // let numsObservable$ = from(nums);
-
-// let evenNumbers$ = Observable.create(subscriber => {
-
-//   for (let currentNum of nums) {
-//     if (currentNum % 2 === 0) {
-//       subscriber.next(currentNum);
-//     }
-//     else {
-//       subscriber.error('Value is not even.');
-//     }
-//   }
-
-//   subscriber.complete();
+  subscriber.complete();
   
-// });
+});
 
-// evenNumbers$.subscribe(
-//   (value) => console.log(value),
-//   (err) => console.log(`ERROR: ${err}`),
-//   () => console.log('All done.')  
-// );
+evenNumbers$.subscribe(
+  (value) => console.log(value),
+  (err) => console.log(`ERROR: ${err}`),
+  () => console.log('All done.')  
+);
